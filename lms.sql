f71fd18df0803c3f6b28a22d938b00ccf532643b
FAHIM HOSSAIN <112601466+faheemashraf23@users.noreply.github.com>
2025-04-12 21:45:37 +0600

    Add files via upload

---
+++ lms.sql
@@ -0,0 +1,231 @@
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `mobile` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `name`, `email`, `password`, `mobile`) VALUES
+(1, 'Fahim', 'fahim15-4375@diu.edu.bd', '235689', 1956000049);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authors`
+--
+
+CREATE TABLE `authors` (
+  `author_id` int(11) NOT NULL,
+  `author_name` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `authors`
+--
+
+INSERT INTO `authors` (`author_id`, `author_name`) VALUES
+(101, 'Md. Hasanuzzaman Dipu'),
+(102, 'Tapasy Rabeya'),
+(103, 'Abdullah Al Kafi'),
+(104, 'Abdullah Al Shafi'),
+(105, 'Abdus Sattar'),
+(106, 'Syeda Maria Rahman'),
+(107, 'Ms. Fatama Jannat Tisha');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `book_id` int(11) NOT NULL,
+  `book_name` varchar(250) NOT NULL,
+  `author_id` int(11) NOT NULL,
+  `cat_id` int(11) NOT NULL,
+  `book_no` varchar(250) NOT NULL,
+  `book_price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`book_id`, `book_name`, `author_id`, `cat_id`, `book_no`, `book_price`) VALUES
+(1, 'Web Engineering', 101, 2, 'CSE-414', 670),
+(2, 'Computer Graphics', 102, 2, 'CSE-421', 470),
+(3, 'Natural Language Processing', 103, 2, 'CSE-445', 470),
+(4, 'Web Engineering Lab', 101, 2, 'CSE-415', 770),
+(5, 'Computer Graphics Lab', 102, 2, 'CSE-422', 270),
+(6, 'Information Security', 104, 2, 'CSE-423', 370),
+(7, 'Software Engineering', 105, 2, 'CSE-333', 640);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `cat_id` int(11) NOT NULL,
+  `cat_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`cat_id`, `cat_name`) VALUES
+(1, 'Faculty of Business and Entrepreneurship (FBE)'),
+(2, 'Faculty of Science and Information Technology (FSIT)'),
+(3, 'Faculty of Engineering (FE)'),
+(4, 'Faculty of Health and Life Sciences (FHLS)'),
+(5, 'Faculty of Humanities and Social Sciences (FHSS)');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `issued_books`
+--
+
+CREATE TABLE `issued_books` (
+  `s_no` int(11) NOT NULL,
+  `book_no` varchar(200) NOT NULL,
+  `book_name` varchar(200) NOT NULL,
+  `book_author` varchar(200) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `issue_date` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `issued_books`
+--
+
+INSERT INTO `issued_books` (`s_no`, `book_no`, `book_name`, `book_author`, `student_id`, `status`, `issue_date`) VALUES
+(1, 'CSE331', 'Compiler Design', 'Ms. Fatama Jannat Tisha', 4375, 1, '2025-04-22'),
+(18, 'CSE332', 'Compiler Design Lab', 'Ms. Fatama Jannat Tisha', 4375, 2, '2025-04-22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `mobile` int(10) NOT NULL,
+  `address` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `mobile`, `address`) VALUES
+(2, 'faheem', 'faheem23@gmail.com', '235689', 1902260748, 'Dattapara, Ashulia , Dhaka'),
+(3, 'akash', 'akash23@gmail.com', '235689', 1956000049, 'Khagan-Bazer, Ashulia , Dhaka');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `authors`
+--
+ALTER TABLE `authors`
+  ADD PRIMARY KEY (`author_id`);
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`book_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indexes for table `issued_books`
+--
+ALTER TABLE `issued_books`
+  ADD PRIMARY KEY (`s_no`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `authors`
+--
+ALTER TABLE `authors`
+  MODIFY `author_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=107;
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `issued_books`
+--
+ALTER TABLE `issued_books`
+  MODIFY `s_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
